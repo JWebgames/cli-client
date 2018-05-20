c27e57ea1bd67ac3d52e1faf371e3f95078ec0db
Julien00859 <julien.castiaux@gmail.com>
2018-05-20 22:13:36 +0200

    move APIError here, add UrwidHandler, add find util

--- tools.py
+++ tools.py
@@ -1,7 +1,15 @@
+import asyncio
 from functools import wraps
-from logging import getLogger
+from logging import getLogger, Handler
+from tenacity import TryAgain
+import view
 
 logger = getLogger(__name__)
+
+
+class APIError(Exception):
+    def __init__(self, status, reason):
+        super().__init__(status, reason)
 
 
 def async_tryexcept(func):
@@ -9,6 +17,27 @@ def async_tryexcept(func):
     async def wrapped(*args, **kwargs):
         try:
             return await func(*args, **kwargs)
-        except:
-            logger.exception("Error in async function")
+        except APIError as exc:
+            if exc.args[0] == 500:
+                logger.exception("%s, see logs for details.", str(exc))
+                raise
+            logger.error("%d: %s", *exc.args)
+        except Exception as exc:
+            logger.exception("%s, see logs for details.", str(exc))
+            raise
     return wrapped
+
+
+class UrwidHandler(Handler):
+    def emit(self, record):
+        msg = self.format(record)
+        try:
+            view.footer.set_text(msg[:msg.index("\n")])
+        except ValueError:
+            view.footer.set_text(msg)
+
+def find(key, iterable):
+    for item in iterable:
+        if key(item):
+            return item
+    return None

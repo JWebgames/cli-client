2c535e36a2c3fbea8bde16774669b0ce1916ffd1
Julien00859 <julien.castiaux@gmail.com>
2018-05-20 22:12:57 +0200

    add log handler logging on terminal

--- __main__.py
+++ __main__.py
@@ -3,12 +3,18 @@
 import logging
 from controller import main
 from config import LOGLEVEL
+from tools import UrwidHandler
 
 logfile = logging.FileHandler("client.log", mode="w")
 logfile.formatter = logging.Formatter(
     "{asctime} [{levelname}] <{name}:{funcName}> {message}", style="{")
 logfile.level = LOGLEVEL
-logging.root.handlers = [logfile]
+
+urwidhdl = UrwidHandler()
+urwidhdl.formatter = logging.Formatter("{message}", style="{")
+urwidhdl.level = logging.INFO
+
+logging.root.handlers = [logfile, urwidhdl]
 logging.root.level = LOGLEVEL
 
 main()
